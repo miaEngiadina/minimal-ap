3610eeffb5a7e0a36c1ae4d15b464cc9e6e3e9e8
pukkamustard <pukkamustard@posteo.net>
2019-09-13 21:45:24 +0200

    objects and implement GET on inbox and outbox

--- main.scm
+++ main.scm
@@ -2,55 +2,114 @@
              (web request)
              (web response)
              (web uri)
+             (json)
              (srfi srfi-9)
              (ice-9 match))
 
+
 ;; Config
 
-(define base-url "http://localhost:8080/")
+(define base-url
+  (uri->string (build-uri 'http #:host "localhost" #:port 8080)))
 
 
-
 ;; Actors
 
 (define-record-type <actor>
-  (make-actor id type inbox outbox)
+  (make-actor id name type inbox outbox)
   actor?
   (id actor-id)
+  (name actor-name)
   (type actor-type)
   (inbox actor-inbox set-actor-inbox!)
   (outbox actor-outbox set-actor-outbox!))
 
-(define (actor->json actor)
-  `(@ ,@activitystreams-context
-      (id ,(actor-id actor))
-      (type ,(actor-type actor))
-      (inbox ,(string-append (actor-id actor) "inbox"))
-      (outbox ,(string-append (actor-id actor) "outbox"))
-      ))
+(define (actor-outbox-uri actor)
+  (string-append
+   (actor-id actor)
+   "/outbox"))
 
-(define alice
+(define (actor-inbox-uri actor)
+  (string-append
+   (actor-id actor)
+   "/inbox"))
+
+(define (actor->scm actor)
+  `(("id" . ,(actor-id actor))
+    ("name" . ,(actor-name actor))
+    ("type" . ,(actor-type actor))
+    ("inbox" . ,(actor-inbox-uri actor))
+    ("outbox" . ,(actor-outbox-uri actor))))
+
+(define (actor-add-to-outbox! actor to-add)
+  (set-actor-outbox! actor (cons to-add (actor-outbox actor))))
+
+
+;; Object
+
+(define-record-type <object>
+  (make-object id type alist)
+  object?
+  (id object-id)
+  (type object-type)
+  (alist object-alist))
+
+(define (object->scm object)
+  (cons*
+   `("id" . ,(object-id object))
+   `("type" . ,(object-type object))
+   (object-alist object)))
+
+
+;; OrderedCollection
+
+(define (as-ordered-collection stuff)
+  `(("type" . "OrderedCollection")
+    ("totalItems" . ,(length stuff))
+    ("orderedItems" . ,(list->vector stuff))))
+
+
+
+;; In memory key-value database
+
+(define database '())
+
+(define (db-add! id value)
+  (set! database (acons id value database)))
+
+(define (db-get id)
+  (assoc-ref database id))
+
+(define (db-add-actor! actor)
+  (db-add! (actor-id actor) actor))
+
+(define (db-dereference id)
+  "Attempt to dereference objects by id from database.
+  If object is not in db just return the id."
+  (let ((dereferenced-thing (db-get id)))
+    (if dereferenced-thing
+        dereferenced-thing
+        id)))
+
+;; Add alice
+(db-add-actor!
   (make-actor
-   (string-append base-url "actors/alice/")
+   (string-append base-url "/actors/" "alice")
+   "Alice"
    "Person"
    '()
    '()))
 
-(define actors
-  `(("alice" . ,alice)))
-
 
-
 ;; JSON helpers
 
-(define activitystreams-context
-  '(("@context" "https://www.w3.org/ns/activitystreams")))
-
-
+(define* (respond-with-json obj #:key (code 200))
+  (values (build-response #:code code
+                          #:headers '((content-type . (application/json))))
+          (lambda (port) (scm->json obj port))))
 
 
-
-;; Server
+;; Web server
 
 (define (not-found request)
   (values (build-response #:code 404
@@ -58,24 +117,46 @@
           (string-append "Resource not found: "
                          (uri->string (request-uri request)))))
 
-(define (activitypub-actors-handler actor-name actor-path-components request request-body)
+(define (activitypub-actors-handler actor-path-components request request-body)
   "Handle requests for a specific actor"
-  (display actor-name)
-  (let ((actor (assoc-ref actors actor-name)))
-    (display actor)
+  (let*
+      (;; build the actor id from the request url
+       (actor-id (string-append base-url "/actors/" (car actor-path-components)))
+       ;; get actor from db
+       (actor (db-get actor-id)))
+
     (if (actor? actor)
-        (values (build-response #:code 200 #:headers '((content-type . (application/json))))
-                (lambda (port) (write-json (actor->json actor) port)))
+
+        (match `(,(request-method request) ,(cdr actor-path-components))
+
+          ;; return the actor object
+          ((GET ())
+           (respond-with-json (actor->scm actor)))
+
+          ((GET ("inbox"))
+           (respond-with-json (as-ordered-collection
+                               (map object->scm (actor-inbox actor)))))
+
+          ((GET ("outbox"))
+           (respond-with-json (as-ordered-collection
+                               (map object->scm (actor-outbox actor)))))
+
+          (_ (not-found request)))
+
+        ;; if actor does not exist respond with 404
         (not-found request))))
+
 
 (define (activitypub-handler request request-body)
   "ActivityPub requests handler"
   (let
-      (;; decode the request path
-       (request-path-components
+      ((request-path-components
         (split-and-decode-uri-path (uri-path (request-uri request)))))
 
-    (display (string-append (uri->string (request-uri request)) "\n"))
+    ;; log the request
+    (display (string-append
+              (symbol->string (request-method request)) " "
+              (uri->string (request-uri request)) "\n"))
 
     (match request-path-components
 
@@ -85,16 +166,14 @@
       (("test")
        (values '((content-type . (text/plain))) "Test"))
 
-      (("actors" actor-name . actor-path-components)
-       (activitypub-actors-handler actor-name actor-path-components request request-body))
+      (("actors" . actor-path-components)
+       (activitypub-actors-handler actor-path-components request request-body))
 
-      (_
-       (not-found request))
+      (_ (not-found request))
 
       )))
 
 
-
 ;; Run it!
 
 (define (main)
